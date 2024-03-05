207cc271d372cbb6d77a0d41cac7a517fa1cad5f
abc-hi <revathimohancse@gmail.com>
2024-03-05 03:47:17 -0800

    2nd update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { format } from 'date-fns';
 const app = express();
 const PORT = 4000;
 app.get('/',(req,res)=>{
-    res.send("application is running successfully")
+    res.send("application is running successfully",`${today}`)
 });
 
 app.get('/write', (req, res) => {
