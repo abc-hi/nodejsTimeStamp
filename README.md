9a379537c8d30019a650c499c39de4167ecb8444
abc-hi <revathimohancse@gmail.com>
2024-03-05 00:41:49 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+*** Here I have created APIs to read and write current timestamp value***
+
+APIs created:
+
+1. Get API To read
+
+2. Get API To Write
+
+
+
+  By importing date-fns, and by using current timestamp, file has been written and read with current time format.
+
---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path'; // Import the path module
+import { format } from 'date-fns';
+
+const app = express();
+const PORT = 4000;
+
+app.get('/write', (req, res) => {
+    let today = format(new Date(), 'dd-mm-yyyy-HH-mm-ss');
+    const filePath = `TimeStamp/${today}.txt`;
+    fs.writeFileSync(filePath, `${today}`, 'utf8');
+    res.send('File written successfully.');
+});
+
+app.get('/read', (req, res) => {
+    let today = format(new Date(), 'dd-mm-yyyy-HH-mm-ss');
+    const filePath = `TimeStamp/${today}.txt`;
+
+    // Use fs.readdir to read the directory
+    fs.readdir('TimeStamp', (err, files) => {
+        if (err) {
+            console.error('Error reading directory:', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        // Filter out only text files
+        const textFiles = files.filter((file) => path.extname(file) === '.txt');
+
+        if (textFiles.length === 0) {
+            res.status(404).send('No text files found.');
+            return;
+        }
+
+        res.status(200).json(textFiles);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
